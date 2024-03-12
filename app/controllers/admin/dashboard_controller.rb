91a5d240921f1f8035f8303cc2f26277d3f2de33
Paul Formby <paulformby@icloud.com>
2024-03-12 22:44:47 +0000

    FIX - fix/admin-basic-auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
   def show
+  end
+  
+  private 
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "Jungle" && password == "book"
+    end
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  before_action :authenticate
+
   def index
     @products = Product.order(id: :desc).all
   end
@@ -35,6 +37,12 @@ class Admin::ProductsController < ApplicationController
       :image,
       :price
     )
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "Jungle" && password == "book"
+    end
   end
 
 end
