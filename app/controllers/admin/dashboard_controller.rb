27f12f8181f7b8c20f7f1627c179f3ce569b1fbe
Paul Formby <paulformby@icloud.com>
2024-03-13 05:48:54 +0000

    ADD - feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,12 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
   def show
   end
+
+  def show
+    @product_count = Product.count
+    @category_count = Category.count
+  end
+
   
   private 
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show, :index]
   end
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -71,4 +72,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
